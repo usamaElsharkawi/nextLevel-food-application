0ce4bf23acf260774f3c97d87f556c18e1545c78
Usama El-Sharkawi <usamasharkawi@gmail.com>
2026-05-06 07:48:10 +0300

    feat: implement meal details page and image picker with preview logic

--- src/app/meals/[mealSlug]/page.js
+++ src/app/meals/[mealSlug]/page.js
@@ -1,4 +1,41 @@
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+
 export default async function MealDetailsPage({ params }) {
-  const { mealSlug } = await params;
-  return <h1>Meal Details Page {mealSlug}</h1>;
+  const {mealSlug} = await params;
+  const meal = getMeal(mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
---
+++ src/components/meals/image-picker.js
@@ -0,0 +1,54 @@
+"use client";
+import classes from "./image-picker.module.css";
+import { useRef, useState } from "react";
+import Image from "next/image";
+
+export default function ImagePicker({ label, name }) {
+  const inputRef = useRef(null);
+  const [pickedImage, setPickedImage] = useState(null);
+
+  function handlePickClick() {
+    inputRef.current.click();
+  }
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+    const filePreview = URL.createObjectURL(file);
+    setPickedImage(filePreview);
+    console.log(file);
+    console.log(filePreview);
+  }
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          name={name}
+          accept="image/*"
+          required
+          ref={inputRef}
+          onChange={handleImageChange}
+        />
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image src={pickedImage} alt="The image you selected" fill />
+          )}
+        </div>
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an image
+        </button>
+      </div>
+    </div>
+  );
+}
